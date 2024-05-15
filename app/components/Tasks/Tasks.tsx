ed8ffe6ca46204e8a0c5f050757212400c61ff66
Ankit Mishra <ankit.mishra@paavu.com>
2024-05-15 18:59:39 +0530

    task page

--- app/components/Tasks/Tasks.tsx
+++ app/components/Tasks/Tasks.tsx
@@ -1,7 +1,11 @@
 "use client";
 import React from "react";
+import { styled } from "styled-components";
 
 function Tasks() {
   return <div>Tasks</div>;
 }
+
+const TaskStyled = styled.div``;
+
 export default Tasks;
