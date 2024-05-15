f3404883dd3aef7c33634c0795c18d8fc2896c55
Ankit Mishra <ankit.mishra@paavu.com>
2024-05-15 18:27:18 +0530

    pages created

---
+++ app/completed/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+
+function page() {
+  return <div>Completed</div>;
+}
+export default page;
---
+++ app/important/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+
+function page() {
+  return <div>Important</div>;
+}
+export default page;
