bcba6cb9f31d5ff0b003ef2e8a03a78d9ea78676
Ankit Mishra <ankit.mishra@paavu.com>
2024-05-16 10:29:31 +0530

    task page

--- app/components/Tasks/Tasks.tsx
+++ app/components/Tasks/Tasks.tsx
@@ -1,11 +1,22 @@
 "use client";
+import { useGlobalState } from "@/app/context/GlobalProvider";
 import React from "react";
 import { styled } from "styled-components";
 
 function Tasks() {
-  return <div>Tasks</div>;
+  const { theme } = useGlobalState();
+
+  return <TaskStyled theme={theme}>Tasks</TaskStyled>;
 }
 
-const TaskStyled = styled.div``;
+const TaskStyled = styled.div`
+  width: 100%;
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+  padding: 2rem;
+  border-radius: 1rem;
+
+  overflow-y: auto;
+`;
 
 export default Tasks;
