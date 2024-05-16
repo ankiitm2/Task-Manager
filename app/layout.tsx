6fab1730a05ef282b82dbb9043fd387237bd463e
Ankit Mishra <ankit.mishra@paavu.com>
2024-05-16 18:53:46 +0530

    signup page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import "./globals.css";
 import Sidebar from "./components/sideBar/Sidebar";
 import GlobalStyledProvider from "./providers/GlobalStyledProvider";
 import ContextProvider from "./context/ContextProvider";
-import { ClerkProvider } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,29 +17,25 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { userId } = auth();
-
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-            integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
-        </head>
-        <body className={inter.className}>
-          <ContextProvider>
-            <GlobalStyledProvider>
-              {userId && <Sidebar />}
-              <div className="w-full">{children}</div>
-            </GlobalStyledProvider>
-          </ContextProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <body className={inter.className}>
+        <ContextProvider>
+          <GlobalStyledProvider>
+            <Sidebar />
+            <div className="w-full">{children}</div>
+          </GlobalStyledProvider>
+        </ContextProvider>
+      </body>
+    </html>
   );
 }
